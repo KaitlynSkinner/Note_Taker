9cf46eda621a32382b8aee3023b56cc20d1b081d
Kaitlyn Skinner <kskinnerr@hotmail.com>
2022-03-13 20:53:09 -0400

    POST added

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
 // Require express package
 const express = require('express');
-// // Initialize app variable by setting it to the value of express()
-const app = express();
 // Require file system
 const fs = require('fs');
-const { title } = require('process');
-// Require the JSON file and assign it to a variable called `dataBase`
-const dataBase = require('./db/db.json');
+// Require path module - handle and transform file path
+const path = require('path');
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PROCESS || 3000;
+// Initialize app variable by setting it to the value of express()
+const app = express();
+
+//const { title } = require('process');
+// Require the JSON file and assign it to a variable called `dataBase`
+//const dataBase = require('./db/db.json');
 
 let notesArr = [];
 
 // Express middleware 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON
 app.use(express.json());
-app.unsubscribe(express.urlencoded({ extended: true }));
+// serve static files 
 app.use(express.static('public'));
 
 // GET method route for notes.html
@@ -28,55 +34,68 @@ app.get("/notes", (req, res) => {
 
 // GET method route for index.html
 app.get('/*/', (req, res) => {
-    // res.json(`${req.method} request received to get index.html`)
-    // console.log(`${req.method} request received to get index.html`);
-    
     res.sendFile(__dirname + '/public/index.html');
 });
 
-// GET request for /api/notes
+// GET request for /api/notes - server requesting clinet to accept data
 app.get('/api/notes', (req, res) => {
-    // let results = dataBase;
-    // if (req.query) {
-    //     results = 
-    // }
     fs.readFile('db/db.json', 'utf8', function(err, data) {
         // Display the file content
         console.log(data);
 
+        //Concatenate note to array
         notesArr = [].concat(JSON.parse(data));
         res.json(JSON.parse(data));
     });
     // should read the db.json file and return all saved notes as JSON.
 });
 
-// POST method route for /api/notes (post to homepage)
+// POST method route for /api/notes (post to homepage) - client requesting the server to accept data
 app.post('/api/notes', (req, res) => {
-    // confirm POST request received
-    console.info(`${req.method} request for notes`);
 
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+
+    // If all the required properties are present..
     if (title && text) {
-        //should receive a new note to save on the request body, 
+        // Variable to receive a new note - to save on the request body, 
         const newNote = {
             title,
             text,
             review_id: uuid(),
         };
 
+        // Obtain exisiting notes
+        fs.readFile('db/db.json', notesArr, function(err, data) {
+            if(err) {
+                console.error(err);
+            } else {
+
+            // Add a new note
+            notesArr.push(newNote);
+
+            // Write updates notes back to the file
+            fs.writeFile(
+                './db/db.json',
+                JSON.stringify(notesArr, null, 4),
+                (writeErr) =>
+                  writeErr
+                    ? console.error(writeErr)
+                    : console.info('Successfully added note!')
+              );
+            }
+        });
+
         const response = {
             status: 'success',
             body: newNote,
         };
-        fs.writeFile('db/db.json', 'utf8', function(err, data) {
-            //add it to the db.json file,
-            console.log(response);
-            res.json(response); 
-        });
+
+        console.log(response);
+        res.send(response);
     } else {
         res.json('Error in posting note');
     }
-//     //and then return the new note to the client. 
-//     //You'll need to find a way to give each note a unique id when it's saved 
 });
 
 //BONUS
